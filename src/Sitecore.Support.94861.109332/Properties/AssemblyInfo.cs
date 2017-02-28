7c7fcf5e22d8f5348d153cc1469e2f59819771aa
Dmitry Kolinchuk <dmko@sitecore.net>
2017-02-28 11:01:51 +0200

    Make the Start Date and End Date filters workable for non-English locales

---
+++ src/Sitecore.Support.94861.109332/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94861.109332")]
+[assembly: AssemblyProduct("Sitecore.Support.94861.109332")]
+[assembly: ComVisible(false)]
